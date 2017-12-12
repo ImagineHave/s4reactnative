5c57aef19010350815f6dbfaa3dc3da2d0ac099d
Paul Chappell <paulchappell@Pauls-MacBook-Pro.local>
2017-12-12 13:30:13 +0000

    removing files

--- index_1.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import prayerReducer from './prayer-reducer';
-
-const rootReducer = combineReducers({
-  users: prayerReducer
-});
-
-export default rootReducer;
